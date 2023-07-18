cb7d674148a3e2226dcac76dc90ef0525c48c7c9
NicolaRicci100 <nicola.ricci09@gmail.com>
2023-07-18 16:12:19 +0200

    tabella con gli hotel

--- index.php
+++ index.php
@@ -54,17 +54,36 @@ $hotels = [
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
 </head>
 
-<body>
-
-  <?php foreach ($hotels as $hotel) : ?>
-    <ul><?= $hotel['name'] ?>
-      <li><?= $hotel['description'] ?></li>
-      <li><?= $hotel['parking'] ?></li>
-      <li><?= $hotel['vote'] ?></li>
-      <li><?= $hotel['distance_to_center'] ?></li>
-    </ul>
-  <?php endforeach ?>
-
+<body class="bg-secondary">
+  <div class="container">
+    <h1 class="text-light text-center mt-5">Hotels</h1>
+    <table class="table mt-5 table-dark table-striped shadow-lg border">
+      <thead>
+        <tr>
+          <th scope="col">Nome</th>
+          <th scope="col">Descrizione</th>
+          <th scope="col">Parcheggio</th>
+          <th scope="col">Voto</th>
+          <th scope="col">Distanza dal centro</th>
+        </tr>
+      </thead>
+      <tbody>
+        <?php foreach ($hotels as $hotel) : ?>
+          <tr>
+            <th scope="row"><?= $hotel['name'] ?></th>
+            <td><?= $hotel['description'] ?></td>
+            <?php if ($hotel['parking'] == true) : ?>
+              <td>SI</td>
+            <?php else : ?>
+              <td>NO</td>
+            <?php endif ?>
+            <td><?= $hotel['vote'] ?> &#9734;</td>
+            <td><?= $hotel['distance_to_center'] ?> Km</td>
+          </tr>
+        <?php endforeach ?>
+      </tbody>
+    </table>
+  </div>
 </body>
 
 </html>
